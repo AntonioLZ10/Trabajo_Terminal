86c9bb363955ac8ec194e6a7225bcf281f06132f
AntonioLuna144 <jluna@mexamerik.com>
2025-11-30 21:44:43 -0600

    deploy corregido

--- backend/main.py
+++ backend/main.py
@@ -197,4 +197,4 @@ async def post_recomendar(request: DescripcionRequest):
 
 # Bloque de ejecución para desarrollo local
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
